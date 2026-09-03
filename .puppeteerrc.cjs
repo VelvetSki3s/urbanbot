6b3cdd67d6b69093103b51eae788dcd898fe7c66
VelvetSki3s <131295960+VelvetSki3s@users.noreply.github.com>
2026-09-03 16:07:26 -0400

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Guarda el navegador descargado dentro de la carpeta del proyecto
+  // Fuerza a Puppeteer a guardar el binario dentro del proyecto
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
